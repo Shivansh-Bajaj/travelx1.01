2776d07d9c35e0beba5f445f550cb1b97ce28390
root <root@localhost.localdomain>
2015-10-02 14:39:55 -0400

    start project

---
+++ bus/busutil.py
@@ -0,0 +1,37 @@
+import requests
+
+def clean(text):
+    if text == 'Route Numbers':
+        return 'bus_nos'
+    return text
+
+def table2json(rows):
+    table_data = [[clean(cell.text) for cell in (row("td") or row("th"))]
+                             for row in rows]
+
+    keys = table_data[0]
+    values = table_data[1:]
+    hops = []
+    for value in values:
+        json_dict = {}
+        c = 0
+        for ele in value:
+            json_dict[keys[c]] = ', '.join(ele.split(','))
+            c+=1
+        _from = u'{} Bus Stop, Bangalore'.format(json_dict['From'])
+        _to = u'{} Bus Stop, Bangalore'.format(json_dict['To'])
+        resp = requests.get(
+            'https://maps.googleapis.com/maps/api/directions/json?'
+            'origin={origin}&destination={destination}&key={key}&'
+            'departure_time=now&mode=transit'.format(
+                origin=_from,destination=_to,
+                key='AIzaSyCNxasSekURQXrJ8eOlPEhQK8foxJ__WqE')).json()
+        if resp['status'] == 'OK' and resp['routes'] \
+                and resp['routes'][0]['legs'] and resp['routes'][0]['legs'][0]['distance']:
+            json_dict['distance'] = resp['routes'][0]['legs'][0]['distance']['text']
+            json_dict['duration'] = resp['routes'][0]['legs'][0]['duration']['text']
+        else:
+            json_dict['distance'] = None
+            json_dict['duration'] = None
+        hops.append(json_dict)
+    return hops
---
+++ cab/view.py
@@ -0,0 +1,76 @@
+from django.conf import settings
+if not settings.configured:
+    settings.configure()
+import json
+import urllib
+import simplejson
+import googlemaps
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+import requests
+from lxml import html
+from BeautifulSoup import BeautifulSoup
+#from busutil import *
+googleGeocodeUrl = 'http://maps.googleapis.com/maps/api/geocode/json?'
+#def calcuatefare(n):
+#    if n<=4:
+#        return '100'
+#    else:
+#        return str(100+(13*(n-4)))
+def index(request):
+    return render(request,'cabform.html')
+def cab(request):
+    if request.method != 'POST':
+        return HttpResponseRedirect('/')
+    src = request.POST.get('from')
+    dst = request.POST.get('to')
+    startlat,startlong=get_coordinates(str(src),from_sensor=False)
+    dstlat,dstlong=get_coordinates(str(dst),from_sensor=False)
+#    gdis=googlemaps.Client(key='AIzaSyBcvocj_-OQspLCSu-L6FwNw81ttKbWBxQ')
+#    distance=gdis.distance_matrix(src,dst)
+#    dis=distance['rows'][0]['elements'][0]['distance']['value']/100
+#    price=calcuatefare(dis)
+    lat,long=get_coordinates("J.L.N Marg,Malviya Nagar, Jaipur",from_sensor=False)
+    lat2,long2=get_coordinates("lnmiit, Jaipur",from_sensor=False)
+    url = 'https://api.uber.com/v1/estimates/price'
+    parameters = {
+        'server_token': 'YcSR2FOOfJMtreWpBeDqeyjDmm8Hj1pSh1ZnQP9h',
+        'start_latitude': startlat,
+        'start_longitude': startlong,
+        'end_latitude': dstlat,
+        'end_longitude':dstlong,
+    }
+    response = requests.get(url, params=parameters)
+    data = response.json()
+    name=[]
+    distance=[]
+    high_estimate=[]
+    low_estimate=[]
+    duration=[]
+    j=0
+    for i in data['prices']:
+        name.append(i['localized_display_name'])
+        distance.append(i['distance'])
+        high_estimate.append(i['high_estimate'])
+        low_estimate.append(i['high_estimate'])
+        duration.append((i['duration'])/60)
+        j=j+1
+    return render(request,'cab.html',{'count':j,'name':name,'distance':distance,'high_estimate':high_estimate,'low_estimate':low_estimate,'duration':duration})
+def get_coordinates(query, from_sensor=False):
+    query = query.encode('utf-8')
+    url = 'https://api.uber.com/v1/estimates/price'
+    params = {
+        'address': query,
+        'sensor': "true" if from_sensor else "false"
+    }
+    url = googleGeocodeUrl + urllib.urlencode(params)
+    json_response = urllib.urlopen(url)
+    response = simplejson.loads(json_response.read())
+    if response['results']:
+        location = response['results'][0]['geometry']['location']
+        latitude, longitude = location['lat'], location['lng']
+        print query, latitude, longitude
+    else:
+        latitude, longitude = None, None
+        print query, "<no results>"
+    return latitude, longitude
