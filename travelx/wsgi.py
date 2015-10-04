19cd5201345f34bee579ddf379196ffdbc195596
root <root@localhost.localdomain>
2015-10-04 18:34:50 -0400

    project complete 1.01

--- travelx/wsgi.py
+++ travelx/wsgi.py
@@ -14,11 +14,9 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "travelx.settings")
 
 application = get_wsgi_application()
-<<<<<<< HEAD
 try:
     from dj_static import Cling
     application = Cling(get_wsgi_application())
 except:
     pass
-=======
->>>>>>> 98ad4f5cb0d1e0e19eb4292a20824df88566a645
+
