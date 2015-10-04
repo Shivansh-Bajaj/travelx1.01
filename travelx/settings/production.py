33b9c734d05ed26551b36fd8f1770b535cacce95
root <root@localhost.localdomain>
2015-10-04 06:46:16 -0400

    herokustart

---
+++ travelx/settings/production.py
@@ -0,0 +1,24 @@
+# Parse database configuration from $DATABASE_URL
+import os
+import dj_database_url
+from django.conf import Settings
+
+DATABASES=Settings.DATABASES
+DATABASES['default'] =  dj_database_url.config()
+DEBUG=False
+TEMPLATE_DEBUG=False
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+#STATIC_ROOT = 'staticfiles'
+#STATIC_URL = '/static/'
+
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#)
